699d5102a956c28eca6df8a6680fc41458d69c0c
Branko Cvetanović <cbranko01@gmail.com>
2023-03-07 22:39:42 +0100

    Add event listener for hovering

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const display=document.querySelector(".display");
  for(let i=1;i<=256;i++){
-    let element =document.createElement("div");
-    element.setAttribute("style","width:32px;height:32px;border:2px red solid;")
-    display.appendChild(element);
+    let div =document.createElement("div");
+    div.setAttribute("style","width:32px;height:32px;border:1px grey solid;")
+    div.classList.add("element")
+    display.appendChild(div);
  }
+ const elements=document.querySelectorAll(".element")
+ elements.forEach(element=>element.addEventListener("mouseover",()=>{
+    element.classList.add("red")
+    console.log(element)
+ }))
