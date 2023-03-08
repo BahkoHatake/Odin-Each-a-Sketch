ace5394dbf78a190547e33fa12b6606c64caaaf7
Branko Cvetanović <cbranko01@gmail.com>
2023-03-08 17:21:04 +0100

    Add pixel selector

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-const display=document.querySelector(".display");
- for(let i=1;i<=256;i++){
-    let div =document.createElement("div");
-    div.setAttribute("style","width:32px;height:32px;border:1px grey solid;")
-    div.classList.add("element")
-    display.appendChild(div);
- }
- const elements=document.querySelectorAll(".element")
- elements.forEach(element=>element.addEventListener("mouseover",()=>{
-    element.classList.add("red")
-    console.log(element)
- }))
+const display=document.querySelector(".display"); 
+let grid=document.createElement("div")
+
+function createGrid(pixelsNum){
+   grid.classList.add("grid")
+   display.appendChild(grid)
+   for(let i=1;i<=pixelsNum;i++){
+   let div =document.createElement("div");
+   div.classList.add("element")
+   grid.appendChild(div);
+}}
+function changeColor(){
+   const elements=document.querySelectorAll(".element")
+   elements.forEach(element=>element.addEventListener("mouseover",()=>{
+   element.classList.add("red")
+}))}
+
+createGrid(256)
+
+changeColor()
+
+const pixels=document.querySelector("#pixels")
+pixels.addEventListener("change",()=>{
+   grid.innerHTML = ''
+   let x=pixels.value
+   set(x)
+   createGrid(x*x)
+   changeColor()
+})
+
+const r =document.querySelector(":root")
+function setCssVariable(c){
+   r.style.setProperty("--pixels",`repeat(${c},auto)`)
+}
